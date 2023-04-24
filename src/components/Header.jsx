18e352f223c40ce097ad94d89980be28c57146d7
GuiSposito <guicarlosjoinhas@gmail.com>
2023-04-24 16:31:48 -0300

    Primeiro commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+function Header() {
+    return(
+        <header>
+            <div className="container">
+                <h1 className="text-3xl font-bold ">
+                    Hello world!
+                </h1>
+            </div>
+        </header>
+    );
+}
+
+export default Header;
